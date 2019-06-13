59cfc75c3f949fdc01a823f1a55fa9836e086a51
RickHerzog <51469035+flh4@users.noreply.github.com>
2019-06-12 20:11:00 -0500

    Update student.py

--- student.py
+++ student.py
@@ -2,26 +2,68 @@ class Student:
 	def __init__(self, fname="", lname=""):
 		self.fname = fname
 		self.lname = lname
+		self.address = ''
+		self.status = ''
+		self.phone = ''
+		self.id = ''
+		self.advisor = ''
+		self.medications = []
 		self.courses = []
-		self.grades = {'HW':[], 'tests':[]}
+		self.grades = {'HW':[], 'tests':[], 'quizes':[]}
 		
-	def setfname(self,name):
+	def setFname(self,name):
 		self.fname = name
 
-	def setlname(self,name):
+	def setLname(self,name):
 		self.lname = name
 
-	def getfname(self):
+	def setAddress(self, add):
+		self.address = add
+
+	def setStatus(self, stat):
+		self.status = stat 
+
+	def setPhone(self, ph):
+		self.phone = ph 
+
+	def setId(self, iD):
+		self.id = iD
+
+	def setAdvisor(self, adv):
+		self.advisor = adv
+
+	def getFname(self):
 		return self.fname
 
 	def getlname(self):
 		return self.lname
 	
-	def fullname(self):
+	def getFullName(self):
 		return self.fname+' '+self.lname
 
-	def addHWgrade(self, grade):
-		self.grades['HW'].append(grade)
+	def getAddress(self):
+		return self.address
 
-	def getGrades(self):
-		return self.grades 
+	def getStatus(self):
+		return self.status
+
+	def getCourses(self):
+		return self.courses
+
+	def getGrades(self, x):
+		return self.grades[x][0][1]
+
+	def getMedications(self):
+		return self.medications 
+
+	def addCourse(self, course):
+		self.courses.append(course)
+
+	def addGrade(self, c, grade):
+		self.grades[c].append(grade)
+
+	def addMedication(self, med):
+		self.medications.append(med)
+
+	def removeCourse(self):
+		pass
