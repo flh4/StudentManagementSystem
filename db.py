ab8b5ac3826baed73b56d83a8b8fcc783a91a5d9
RickHerzog <51469035+flh4@users.noreply.github.com>
2019-07-15 00:08:34 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,23 +1,42 @@
 import pymysql.cursors
 
+with open('server.txt') as f:
+    server = f.read().splitlines()
 
-db = pymysql.connect(host='localhost',
-						user='rick',
-						password='Drab$$Wymoop2$',
-						db ='school')
+db = pymysql.connect(host=server[0],
+			user=server[1],
+			password=server[2],
+			db =server[3])
 cursor = db.cursor()
-
 
 def getStudents():
 	s = []
-	cursor.execute("SELECT * FROM faculty")
+	cursor.execute("SELECT * FROM student")
 	d = cursor.fetchall()
 	for i in d:
 		s.append(str(i[1]) + ' ' + str(i[2]))
 	return s
 
+def getFaculty():
+	f = []
+	cursor.execute("SELECT * FROM faculty")
+	d = cursor.fetchall()
+	for i in d:
+		f.append(str(i[1]) + ' ' + str(i[2]))
+	return f
+
 def addFaculty(idd, f, l, dept, title, pswd):
 	query = "INSERT INTO FACULTY VALUES (idd, f, l, dept, title, pswd)"
+
+def DelFaculty():
+	pass
+
+def addStudent():
+	pass
+	#query = "INSERT INTO STUDENT VALUES ()"
+
+def delStudent():
+	pass
 
 def getUserPasswd(user):
 	query = "SELECT passwd FROM faculty WHERE lname= '%s'"
@@ -28,14 +47,8 @@ def getUserPasswd(user):
 	except:
 		print("Error")
 
-def addStudent():
-	pass
-
-def delStudent():
-	pass
-
-
-#db.close()
+def close_db():
+	db.close()
 
 
 
