5a39396594e8835ae763e262ebeb97461ed67624
RickHerzog <51469035+flh4@users.noreply.github.com>
2019-07-15 00:05:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ class App():
 		self.WIDTH = 600
 		self.date = datetime.now().strftime("%B %d, %Y")
 		self.students = getStudents()
+		self.faculty = getFaculty()
 		#Set Canvas
 		self.canvas = tk.Canvas(self.parent, height=self.HEIGHT, width=self.WIDTH).pack()
 		#Initialize Widgets
@@ -43,15 +44,79 @@ class App():
 			self.passwd.delete(0, 'end')
 			messagebox.showerror("Error", "Wrong Username or Password")
 
+	def searchFaculty(self):
+		clicked = self.lb2.curselection()
+		for i in clicked:
+			print(self.lb2.get(i))
+
+	def searchStudents(self):
+		clicked = self.lb1.curselection()
+		for i in clicked:
+			print(self.lb1.get(i))
+
+	def addStudentPage(self):
+		self.multiFrame = tk.Frame(self.parent, bg='green')
+		self.multiFrame.place(relx=.275, rely=.255, relwidth=.5, relheight=.7)
 
 	def initiate_main(self):
+		#Frames
 		self.main = tk.Frame(self.parent, bg='gray')
 		self.main.place(relx=0, rely=.2, relwidth=1, relheight=.8)
-		
+		#ButtonsFrame
+		self.btns = tk.Frame(self.parent, bg='#ffb366')
+		self.btns.place(relx=.8, rely=.2, relwidth=.2, relheight=.8)
+
+		#ListBoxes
+		#StudentBox
+		self.lb1 = tk.Listbox(self.main)
+		self.lb1.place(relx=0, rely=0, relwidth=.25, relheight=.4)
+		#FacultyBox
+		self.lb2 = tk.Listbox(self.main)
+		self.lb2.place(relx=0, rely=.5, relwidth=.25, relheight=.4)
+
+		for s in self.students:
+			self.lb1.insert('end', str(s))
+
+		for f in self.faculty:
+			self.lb2.insert('end', str(f))
+
+		#ScrollBars
+		self.scrollbar1 = tk.Scrollbar(self.lb1, orient="vertical")
+		self.scrollbar1.config(command=self.lb1.yview)
+		self.scrollbar1.pack(side="right", fill="y")
+		self.lb1.config(yscrollcommand=self.scrollbar1.set)
+
+		self.scrollbar2 = tk.Scrollbar(self.lb2, orient="vertical")
+		self.scrollbar2.config(command=self.lb2.yview)
+		self.scrollbar2.pack(side="right", fill="y")
+		self.lb2.config(yscrollcommand=self.scrollbar2.set)
+
+		#Buttons
+		self.search1 = tk.Button(self.main, text="Search Faculty", command=self.searchFaculty)
+		self.search1.place(relx=.02, rely=.92, relwidth=.2, relheight=.05)
+
+		self.search2 = tk.Button(self.main, text="Search Students", command=self.searchStudents)
+		self.search2.place(relx=.02, rely=.42, relwidth=.2, relheight=.05)
+
+		self.addStu = tk.Button(self.btns, text="Add Student", command=self.addStudentPage)
+		self.addStu.place(relx=0, rely=0, relwidth=1, relheight=.1)
+
+		self.removeStu = tk.Button(self.btns, text="Remove Student")
+		self.removeStu.place(relx=0, rely=.1, relwidth=1, relheight=.1)
+
+		self.classes = tk.Button(self.btns, text="My Classes", bg='orange')
+		self.classes.place(relx=0, rely=.2, relwidth=1, relheight=.1)
+
+		self.exit = tk.Button(self.btns, text="Exit Program", bg='red')
+		self.exit.place(relx=0, rely=.87, relwidth=1, relheight=.13)
+
+
 	def addFac(self):
 		self.login.destroy()
+		#Frames
 		self.add = tk.Frame(self.parent, bg='blue')
 		self.add.place(relx=0, rely=.2, relwidth=1, relheight=.8)
+		#Buttons
 		self.back = tk.Button(self.add, text="Back to Login", command=self.LoginPage)
 		self.back.place(relx=0, rely=.85, relwidth=.25, relheight=.15)
 
